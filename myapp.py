9d97365501f646786248e493dc8c0fec65a583cd
Quyen Nguyen <quyennguyen8734@gmail.com>
2023-04-29 22:55:54 -0700

    add fake database and render pages with locations

--- myapp.py
+++ myapp.py
@@ -3,6 +3,35 @@ from flask import Flask, redirect, url_for, render_template, request
 
 app = Flask(__name__)
 acc = {"admin":"admin"}
+data = [
+    {
+        "name":"BCIT",
+        "city": "Vancouver",
+        "address":"555 Seymour",
+        "hours":"08:00 - 17:00",
+        "link":"https://www.bcit.ca/",
+        "phone":"(604) 434-5734",
+        "type":"uni",
+    },
+    {
+        "name":"BCIT",
+        "city": "Burnaby",
+        "address":"3700 Willingdon Ave",
+        "hours":"08:00 - 17:00",
+        "link":"https://www.bcit.ca/",
+        "phone":"(604) 434-5734",
+        "type":"uni",
+    },
+    {
+        "name":"UBC",
+        "city": "Vancouver",
+        "address":"2329 West Mall",
+        "hours":"07:00 - 16:00",
+        "link":"https://www.ubc.ca/",
+        "number":"(604) 822-2211",
+        "type":"uni",
+    },
+]
 
 @app.route("/")
 def home():
@@ -10,27 +39,27 @@ def home():
 
 @app.route("/vancouver")
 def vancouver():
-    return render_template("vancouver.html")
+    return render_template("vancouver.html", locations = data)
 
 @app.route("/burnaby")
 def burnaby():
-    return render_template("burnaby.html")
+    return render_template("burnaby.html", locations = data)
 
 @app.route("/surrey")
 def surrey():
-    return render_template("surrey.html")
+    return render_template("surrey.html", locations = data)
 
 @app.route("/richmond")
 def richmond():
-    return render_template("richmond.html")
+    return render_template("richmond.html", locations = data)
 
 @app.route("/coquitlam")
 def coquitlam():
-    return render_template("coquitlam.html")
+    return render_template("coquitlam.html", locations = data)
 
 @app.route("/locations")
 def locations():
-    return render_template("locations.html")
+    return render_template("locations.html", locations = data)
 
 @app.route("/admin")
 def admin():
