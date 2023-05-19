74b177b5f2148a7a438f5325f99965023ce52bbf
Quyen Nguyen <hnguyen254@my.bcit.ca>
2023-05-18 22:51:18 -0700

    handle 404 url error

--- myapp.py
+++ myapp.py
@@ -273,6 +273,11 @@ def logout():
         session.pop("admin")
     return redirect("/")
 
+# Handles page for 404 error
+@app.errorhandler(404)
+def not_found(e):
+    return render_template("404.html"), 404
+
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
